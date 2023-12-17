f2a61df32105f32382ce3775912110b9486fde7f
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-12-17 20:06:15 +0530

    e-cart

---
+++ src/app/aii-products/aii-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aii-products',
+  templateUrl: './aii-products.component.html',
+  styleUrls: ['./aii-products.component.css']
+})
+export class AiiProductsComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AiiProductsComponent } from './aii-products/aii-products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AiiProductsComponent,
+    ViewProductComponent,
+    LoginComponent,
+    RegisterComponent,
+    WishlistComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
