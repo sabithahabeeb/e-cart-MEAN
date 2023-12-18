534f337e3014a333a3490d9c4852afd4ebe20298
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-12-18 19:25:25 +0530

    cards

--- src/app/aii-products/aii-products.component.ts
+++ src/app/aii-products/aii-products.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-aii-products',
   templateUrl: './aii-products.component.html',
   styleUrls: ['./aii-products.component.css']
 })
-export class AiiProductsComponent {
+export class AiiProductsComponent implements OnInit{
+  allproducts:any=[]
+  constructor(private api:ApiService){}
+
+ngOnInit(): void {
+  this.api.getAllproductsAPI().subscribe((res:any)=>{
+    this.allproducts = res
+  })
+}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RegisterComponent } from './register/register.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -28,7 +29,9 @@ import { CheckoutComponent } from './checkout/checkout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
